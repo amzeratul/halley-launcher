d3a2922c1fa38abe21096e59e12daad25e948998
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2023-03-17 19:56:58 +0000

    Fix update script?

--- scripts/update.bat
+++ scripts/update.bat
@@ -1,8 +1,13 @@
 @echo off
 
+sleep 1 2>NUL
+
 taskkill /IM halley-launcher.exe /F 2>NUL
 
-sleep 1
+sleep 1 2>NUL
 
 robocopy ..\assets ..\..\assets /E /COPYALL /MOVE
 robocopy ..\bin ..\..\bin /E /COPYALL /MOVE
+
+cd ..\..\bin
+start halley-launcher.exe
